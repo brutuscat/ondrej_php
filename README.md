8915566a0efbc51cc583c59ebdc95e8a549979d2
Mauro Asprea <mauroasprea@gmail.com>
2016-10-10 11:57:03 +0200

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# ondrej_php
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: ondrej_php
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
